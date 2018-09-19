4b1701575274dfaf8d782c4f7ae2be750d1aa96a
Ratsimisampy <ratsimisampy@yahoo.fr>
2018-09-19 22:01:13 +0200

    realisation defi n2

--- voyages.rb
+++ voyages.rb
@@ -1,12 +1,18 @@
 #mon 1er programme ruby
-#villes = ['Paris','New York','Berlin','Montreal']
+voyages = [
+    { ville: "Paris", duree: 10 },
+    { ville: "New York", duree: 5 },
+    { ville: "Berlin", duree: 2 },
+    { ville: "Montréal", duree: 15 }
+]
 
-#defi n1: afficher les villes une apres l'aut
+#defi n2: afficher "Voyages a X de Y jours"
 
 i = 0
-villes.each do
+voyages.each do
 
-puts villes[i]
+puts "Voyages a " + voyages[i][:ville]
+puts " de " + voyages[i][:duree].to_s + " jours" 
 i=i+1
 
 end
