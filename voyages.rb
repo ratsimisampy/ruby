e6053978589d5e0f45ea945f441c5abbd5e2c0ee
Ratsimisampy <ratsimisampy@yahoo.fr>
2018-09-19 21:33:54 +0200

    realisation du defi n1

---
+++ voyages.rb
@@ -0,0 +1,13 @@
+#mon 1er programme ruby
+#villes = ['Paris','New York','Berlin','Montreal']
+
+#defi n1: afficher les villes une apres l'aut
+
+i = 0
+villes.each do
+
+puts villes[i]
+i=i+1
+
+end
+
