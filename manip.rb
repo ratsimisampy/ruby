96a39b31ab99e285b9b2bf80857f0b43b1f77da4
Ratsimisampy <ratsimisampy@yahoo.fr>
2018-09-20 10:17:02 +0200

    ajout instance de classe et methode

--- manip.rb
+++ manip.rb
@@ -7,16 +7,29 @@ end
 #user 1
 Bob = Utilisateur.new
 Bob.prenom = "Bob"
+
 #user 2
 Alice = Utilisateur.new
 Alice.prenom = "Alice"
-Alice.amis = ["Bob","Claire"]
+Alice.amis = ["Julien","Claude"]
+
 #user 3
 Claire = Utilisateur.new
 Claire.prenom = "Claire"
 
-puts Alice.prenom + " a " + Alice.amis.size.to_s + " amis."
 
-def est_ami_avec()
-	
-end 
+
+def est_ami_avec(personne)
+	if personne.amis.size > 0
+		i = 0
+		personne.amis.each do
+			puts personne.prenom + " est ami avec "  + personne.amis[i]
+			i = i + 1
+		end
+	else
+		puts personne.prenom + " n'a pas d'amis."
+	end
+end
+
+#puts "Alice a "+ Alice.amis.size.to_s + " amis." 
+puts est_ami_avec(Alice)
