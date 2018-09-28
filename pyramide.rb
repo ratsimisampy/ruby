21fc17a49793c92e77510c7bd3612936a45ef4c6
ratsimisampy <ratsimisampy@gmail.com>
2018-09-28 11:32:27 +0200

    last version

--- pyramide.rb
+++ pyramide.rb
@@ -5,7 +5,6 @@ n = gets.chomp.to_i
 if n > 0 && n < 26
 i = 0
 res = ""
-
 ##==Etape 1  ============
 # -----> tab
 #
@@ -25,23 +24,20 @@ res = ""
 empty_char = "*"
 tab_empty = Array.new
 tab = Array.new
-	n.times do
+n.times {
 		res = res + "#"
-		puts "Dans tab #{i} on aura "  + res
+	#	puts "Dans tab #{i} on aura "  + res
 		tab[i]=res
-	# **********
 		s = ""
 		n2 = (n - i)-1
-		n2.times do s = s + empty_char end 
-# puts s	# log			
+		n2.times { s = s + empty_char } 			
 				tab_empty[i]= s		
-		puts "Dans tab_empty #{i} on a: " + s
+	#	puts "Dans tab_empty #{i} on a: " + s
 		i = i + 1
-
-	end
-puts " "
-puts "On re ecrit dans l'ordre inversee pour concatener tab[] a tab_empty[]  "
-puts " "
+	}
+#puts " "
+#puts "On re ecrit dans l'ordre inversee pour concatener tab[] a tab_empty[]  "
+#puts " "
 ##==Etape 2  ============
 # ------> on reécrit dans le bon ordre : tab_empty + tab , n fois
 #	****#
@@ -51,14 +47,13 @@ puts " "
 #	#####
 ###=====================
 j = 0 
-n.times do
+n.times {
 	puts tab_empty[j] + tab[j] 
 	j = j + 1		
-end
+}
 else
 	puts "Erreur de saisie!"
-
 end
 
-print ""
+puts "Dans un souci didactique les espaces ont ete remplace par des '*'"
 puts "You win!!! just need code optimization !!!"
